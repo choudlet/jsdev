93f4e6d179626b1e7558ce4219d0919c3ca27e8f
Chris Houdlette <choudlette@gmail.com>
2017-04-17 14:47:31 -0600

    completed

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -15,9 +15,19 @@ app.use(require('webpack-dev-middleware')(compiler, {
   publicPath: config.output.publicPath
 }))
 
+
+
 app.get('/',(req,res)=>{
 res.sendFile(path.join(__dirname, '../src/index.html'));
 })
+
+app.get('/users', function(req,res) {
+  res.json([
+    {"id": 1, "firstName": "Bob", "lastName": "Smith", "email":"bob@gmail.com"},
+    {"id": 2, "firstName": "Tammy", "lastName": "Norton", "email":"tnorton@yahoo.com"},
+    {"id": 3, "firstName": "Tina", "lastName": "Lewis", "email":"tina@hotmail.com"},
+  ])
+});
 
 app.listen(port, (err)=>{
   if(err) {
